6dccbd730f0a2a8835566a5f28292d8ea1d66df0
chernykhev <chernykhev@it2g.ru>
2022-01-13 12:05:51 +0500

    Добавление startup проекта и проекта для работы с бд

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class DataContext : DbContext
+{
+    /// <summary>
+    /// Контекст данных для системы дистанционного обучения
+    /// </summary>
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+    }
+}
