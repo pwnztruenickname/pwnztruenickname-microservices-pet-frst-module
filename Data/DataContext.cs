875bb1afd1312013227dd86ca90d57218ba37cab
chernykhev <chernykhev@it2g.ru>
2022-01-18 10:55:03 +0300

    опустил версию .net до 5-ой

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,20 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Data;
-
-public class DataContext : DbContext
+namespace Data
 {
-    /// <summary>
-    /// Контекст данных для системы дистанционного обучения
-    /// </summary>
-    public DataContext(DbContextOptions<DataContext> options)
-        : base(options)
+    public class DataContext : DbContext
     {
-    }
+        /// <summary>
+        /// Контекст данных для системы дистанционного обучения
+        /// </summary>
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
     
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
     }
 }
